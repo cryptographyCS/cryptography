accbde132f02bddc581ff1d364d826d6b325df1e
mikeg <mikeg@steelhouse.com>
2018-01-29 14:19:21 -0800

    clean up

--- client/index.js
+++ client/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './store';
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
