f71f1c893351e2f485ad1c8ba3731fdc3f336111
Tom Rosenblatt <rosenblatttom@gmail.com>
2018-01-29 19:13:18 -0800

    updated UI

--- client/app.js
+++ client/app.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Switch, Route } from 'react-router-dom'
 import Login from './components/Login';
-// import Portfolio from './components/Portfolio';
+import Portfolio from './components/Portfolio';
 import './style.css';
 
 class App extends Component {
   render() {
     return (
-      <div id="app">
-        <Login />
-        {/* <Portfolio /> */}
-      </div>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Login}/>
+          <Route path='/portfolio' component={Portfolio}/>
+          {/* <Route path='/settings' component={Settings}/> */}
+        </Switch>
+      </main>
     );
   }
 }
--- client/components/Login.js
+++ client/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Login extends Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class Login extends Component {
       password: '',
     };
   }
-
+  
   handleUser() {
     this.setState({ user: event.target.value });
   }
@@ -16,14 +17,31 @@ class Login extends Component {
   handlePassword() {
     this.setState({ password: event.target.value });
   }
+  
+  submitForm(event) {
+    event.preventDeault();
+  }
 
   render() {
     return (
-      <div>
-        <input value={this.state.user} onChange={this.handleUser.bind(this)} type="text" />
-        <input value={this.state.password} onChange={this.handlePassword.bind(this)}type="password" />
+      <div className='login-page'>
+        <div>
+            <Link to='/portfolio'><button id='register'>register</button></Link>
+        </div>
+        <div id='title'>
+          cryptography
+        </div>
+        <div id='description'>
+          track your cryptocurrency portfolio in one place 
+        </div>
+        <form className='login' onSubmit={this.submitForm}>
+          <input id="username" value={this.state.user} onChange={this.handleUser.bind(this)} type="text"  placeholder="username"/>
+          <input id="password" value={this.state.password} onChange={this.handlePassword.bind(this)}type="password"  placeholder="password"/>
+          <Link to='/portfolio'><button id='submit' type='submit'>submit</button></Link>
+        </form>
       </div>
     );
   }
 }
+
 export default Login;
