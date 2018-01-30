2e1f614744b76b2d7af29a75f33789c6612dc366
ryanlondon <ryel590@gmail.com>
2018-01-29 20:03:18 -0800

    added auth controller and sql integration

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const pg = require('pg');
 
 const authController = require('./controllers/authController');
 const apiController = require('./controllers/apiController');
@@ -15,5 +13,16 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.post('api/login', authController.login, (req, res) => {
+  res.send('Login');
+});
+
+app.post('api/signup',
+  authController.getUser,
+  authController.validateUser,
+  (req, res) => {
+    res.send('Signup');
+});
 
 app.listen(3000);
